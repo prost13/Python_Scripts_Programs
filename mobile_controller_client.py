6f9acd2437b1c0c889c262bf80dc70c2465c47ba
prost13 <87657820+prost13@users.noreply.github.com>
2021-10-06 17:40:45 +0300

    Update mobile_controller_client.py

--- mobile_controller_client.py
+++ mobile_controller_client.py
@@ -1,7 +1,7 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((192.168.100, 4444))
-client.connect(('192.168.100', 4444))
+client.connect((192...., 4444))
+client.connect(('192.....', 4444))
 
 while True:
   client.send(input().encode("utf-8))
