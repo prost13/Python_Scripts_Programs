b79fc677dc1462420d36c5630b4642215bf8316c
prost13 <87657820+prost13@users.noreply.github.com>
2021-10-06 17:34:35 +0300

    Create papyrus_encryption.py

---
+++ papyrus_encryption.py
@@ -0,0 +1,18 @@
+class Encryptor:
+    def __init__(self):
+        self.key = 20
+
+    def encrypt(self, text):
+        encrypted_text = ""
+
+        for symb in text:
+          encrypted_text += chr(ord(symb) + self.key)
+        return encrypted_text
+
+    def decrypt(self, text):
+        decrypted_text = ""
+
+        for symb in text:
+          decrypted_text += chr(ord(symb) - self.key)
+        return decrypted_text
+
